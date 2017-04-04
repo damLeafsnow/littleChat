e6c84268e4d35a891e8453d8d0a171eacfb721ed
damLeafsnow <llzzsszfy001@gmail.com>
2017-04-05 01:40:21 +0800

    test py

--- tests/dataPickling.py
+++ tests/dataPickling.py
@@ -1,20 +1,20 @@
-try:
+﻿try:
     import cPickle as pickle
 except ImportError:
     import pickle
 import os
 import json
 
-localFile = pathname = os.path.split(os.path.realpath(__file__))[0]
+localPath = os.path.split(os.path.realpath(__file__))[0]
 
 d = dict(name = 'asahi', age = '17', score = 95)
 d2 = {'name' : 'luna', 'age' : 16, 'score' : 100}
 
-with open(''.join(localFile) + '\\dataSave.txt', 'wb') as f:
+with open(''.join(localPath) + '\\dataSave.txt', 'wb') as f:
     pickle.dump(d, f)
     pickle.dump(d2, f)
     
-with open(''.join(localFile) + '\\dataSave.txt', 'rb') as f2:
+with open(''.join(localPath) + '\\dataSave.txt', 'rb') as f2:
     d3 = pickle.load(f2)
     d4 = pickle.load(f2)
 
@@ -34,10 +34,10 @@ def json2MsgUnit(d):
         return MsgUnit(d['time'], d['userID'], d['msgType'], d['msgContent'])
 
 testMsg = MsgUnit('<2017.04.05 00:18:50>', 'asuna', 'public', 'hello,everyone')
-with open(''.join(localFile) + '\\classSave.json', 'wb') as f3:
+with open(''.join(localPath) + '\\classSave.json', 'wb') as f3:
         json.dump(testMsg, f3, default = lambda obj: obj.__dict__)
         
-with open(''.join(localFile) + '\\classSave.json', 'rb') as f4:
+with open(''.join(localPath) + '\\classSave.json', 'rb') as f4:
         str = f4.readline()
 testMsg = json.loads(str, object_hook = json2MsgUnit)
 print testMsg.time + testMsg.userID + ':' + testMsg.msgContent
---
+++ tests/msgsavetest.py
@@ -0,0 +1,41 @@
+﻿try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
+import os
+import time
+localPath = os.path.split(os.path.realpath(__file__))[0]
+
+msg = {
+    'time' : time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+    'username' : 'luna',
+    'msgType' : 'public',
+    'msgContent' : u'测试一下中文'
+    }
+    
+msg2 = {
+    'time' : time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+    'username' : 'asahi',
+    'msgType' : 'private',
+    'msgContent' : u'日本のテスト'
+    }
+
+def printMsg(message):
+    if message['msgType'] == 'public':
+        print '<' + message['time'] + '>' + message['username'] + ':' + message['msgContent']
+    elif message['msgType'] == 'private':
+        print '[Pvt]' + '<' + message['time'] + '>' + message['username'] + ':' + message['msgContent']
+    
+#printMsg(msg)
+
+with open(''.join(localPath) + '\\msgdata', 'w') as f:
+    pickle.dump(msg, f)
+    pickle.dump(msg2, f)
+    
+with open(''.join(localPath) + '\\msgdata', 'r') as f2:
+    while True:
+        try:
+            d = pickle.load(f2)
+            printMsg(d)
+        except EOFError:
+            break
